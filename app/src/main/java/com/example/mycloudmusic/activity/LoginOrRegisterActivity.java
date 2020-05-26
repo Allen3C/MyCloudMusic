da4480e806877b463d722eb5c51acc9fa95df14a
Allen3C <allen3c@163.com>
2020-05-26 22:56:05 +0800

    微博第三方登录

--- app/src/main/java/com/example/mycloudmusic/activity/LoginOrRegisterActivity.java
+++ app/src/main/java/com/example/mycloudmusic/activity/LoginOrRegisterActivity.java
@@ -34,6 +34,7 @@ import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.PlatformDb;
 import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
 import cn.sharesdk.tencent.qq.QQ;
 
 /**
@@ -191,6 +192,61 @@ public class LoginOrRegisterActivity extends BaseCommonActivity {
              * @param platform
              * @param i
              */
+            @Override
+            public void onCancel(Platform platform, int i) {
+                LogUtil.d(TAG, "other login cancel:" + i + "," + HandlerUtil.isMainThread());
+            }
+        });
+
+        //authorize与showUser单独调用一个即可
+        //授权并获取用户信息
+        platform.showUser(null);
+    }
+
+    /**
+     * 微博第三方登录
+     */
+    @OnClick(R.id.iv_weibo)
+    public void onWeiboLoginClick(){
+        //初始化具体的平台
+        Platform platform = ShareSDK.getPlatform(SinaWeibo.NAME);
+
+        //设置false表示使用SSO授权方式
+        platform.SSOSetting(false);
+
+        //回调信息
+        //可以在这里获取基本的授权返回的信息
+        platform.setPlatformActionListener(new PlatformActionListener() {
+
+            /**
+             * 登录成功了
+             * @param platform
+             * @param i
+             * @param hashMap
+             */
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+                //登录成功了
+
+                //就可以获取到昵称，头像，OpenId
+                //该方法回调不是在主线程
+
+                //从数据库获取信息
+                //也可以通过user参数获取
+                PlatformDb db = platform.getDb();
+
+                String nickname = db.getUserName();
+                String avatar = db.getUserIcon();
+                String openId = db.getUserId();
+
+                LogUtil.d(TAG, "other login success:" + nickname + "," + avatar + "," + openId + "," + HandlerUtil.isMainThread());
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+                LogUtil.d(TAG, "other login error:" + throwable.getLocalizedMessage() + "," + HandlerUtil.isMainThread());
+            }
+
             @Override
             public void onCancel(Platform platform, int i) {
                 LogUtil.d(TAG, "other login cancel:" + i + "," + HandlerUtil.isMainThread());
