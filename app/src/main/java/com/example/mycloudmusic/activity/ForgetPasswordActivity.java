5325a6f3ab36aa66a4a52e56bcf48256b0dab35d
Allen3C <allen3c@163.com>
2020-05-27 10:28:23 +0800

    完成找回密码界面布局，并关联点击事件

--- app/src/main/java/com/example/mycloudmusic/activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/mycloudmusic/activity/ForgetPasswordActivity.java
@@ -3,17 +3,51 @@ package com.example.mycloudmusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mycloudmusic.R;
+import com.example.mycloudmusic.util.LogUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 忘记密码界面
  */
-public class ForgetPasswordActivity extends AppCompatActivity {
+public class ForgetPasswordActivity extends BaseTitleActivity {
+
+    private static final String TAG = "ForgetPasswordActivity";
+    @BindView(R.id.et_username)
+    EditText et_username;
+    @BindView(R.id.et_code)
+    EditText et_code;
+    @BindView(R.id.bt_send_code)
+    Button bt_send_code;
+    @BindView(R.id.et_password)
+    EditText et_password;
+    @BindView(R.id.et_confirm_password)
+    EditText et_confirm_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+    }
+
+    /**
+     * 发送验证码按钮点击
+     */
+    @OnClick(R.id.bt_send_code)
+    public void onSendCodeClick() {
+        LogUtil.d(TAG, "onSendCodeClick");
+    }
+
+    /**
+     * 找回密码按钮点击
+     */
+    @OnClick(R.id.bt_forget_password)
+    public void onResetPasswordClick() {
+        LogUtil.d(TAG, "onResetPasswordClick");
     }
 }
