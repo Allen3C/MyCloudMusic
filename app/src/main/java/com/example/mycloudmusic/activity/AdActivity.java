cacf685a5b05e30cca95ba7d8659eb8d075704c3
Allen3C <allen3c@163.com>
2020-05-30 17:17:47 +0800

    实现广告界面布局

--- app/src/main/java/com/example/mycloudmusic/activity/AdActivity.java
+++ app/src/main/java/com/example/mycloudmusic/activity/AdActivity.java
@@ -3,13 +3,22 @@ package com.example.mycloudmusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.mycloudmusic.R;
+import com.example.mycloudmusic.util.LogUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 广告界面（启动界面后的广告）
  */
 public class AdActivity extends BaseCommonActivity {
+
+    private static final String TAG = "AdActivity";
+    @BindView(R.id.bt_skip_ad)
+    Button bt_skip_ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,21 @@ public class AdActivity extends BaseCommonActivity {
         super.initViews();
         //全屏
         fullScreen();
+    }
+
+    /**
+     * 广告点击了
+     */
+    @OnClick(R.id.bt_ad)
+    public void onAdClick() {
+        LogUtil.d(TAG, "onAdClick");
+    }
+
+    /**
+     * 跳过广告按钮
+     */
+    @OnClick(R.id.bt_skip_ad)
+    public void onSkipAd() {
+        LogUtil.d(TAG, "onSkipAd");
     }
 }
