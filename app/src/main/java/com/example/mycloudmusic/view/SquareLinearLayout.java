ecca37e21a78dbcd0f4b7e996954bd3f456c5e8d
Allen3C <allen3c@163.com>
2020-06-05 09:45:37 +0800

    自定义正方形LinearLayout

---
+++ app/src/main/java/com/example/mycloudmusic/view/SquareLinearLayout.java
@@ -0,0 +1,80 @@
+package com.example.mycloudmusic.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+/**
+ * 正方形LinearLayout
+ */
+public class SquareLinearLayout extends LinearLayout {
+    /**
+     * 构造方法
+     *
+     * @param context
+     */
+    public SquareLinearLayout(Context context) {
+        super(context);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param context
+     * @param attrs
+     */
+    public SquareLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param context
+     * @param attrs
+     * @param defStyleAttr
+     */
+    public SquareLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param context
+     * @param attrs
+     * @param defStyleAttr
+     * @param defStyleRes
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SquareLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * 测量控件
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        //设置系统测量好的尺寸
+        setMeasuredDimension(getDefaultSize(0, widthMeasureSpec), getDefaultSize(0, heightMeasureSpec));
+
+        //获取测量后的宽度
+        int width = getMeasuredWidth();
+
+        //创建一个测量规格
+        //设置高度等于宽度
+        widthMeasureSpec = heightMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
+
+        //让父类设置尺寸
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
