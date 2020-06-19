4b13f744088e40a9f3e49d400166edc3bf5e438a
allen <allen3c@163.com>
2020-06-20 03:15:39 +0800

    请求歌单详情数据

--- app/src/main/java/com/example/mycloudmusic/activity/SheetDetailActivity.java
+++ app/src/main/java/com/example/mycloudmusic/activity/SheetDetailActivity.java
@@ -7,14 +7,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mycloudmusic.R;
+import com.example.mycloudmusic.domain.Sheet;
+import com.example.mycloudmusic.domain.response.DetailResponse;
+import com.example.mycloudmusic.listener.HttpObserver;
+import com.example.mycloudmusic.network.Api;
 import com.example.mycloudmusic.util.Constant;
+import com.example.mycloudmusic.util.LogUtil;
 
 /**
  * 歌单详情界面
  */
 public class SheetDetailActivity extends BaseTitleActivity {
 
+    private static final String TAG = "SheetDetailActivity";
+    /**
+     * 歌单id
+     */
     private String id;
+    /**
+     * 歌单
+     */
+    private Sheet data;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,29 @@ public class SheetDetailActivity extends BaseTitleActivity {
 //        //获取传递的参数
 //        id = getIntent().getStringExtra(Constant.ID);
         id = extraId();
+
+        fetchData();
+    }
+
+    private void fetchData() {
+        Api.getInstance()
+                .sheetDetail(id)
+                .subscribe(new HttpObserver<DetailResponse<Sheet>>() {
+                    @Override
+                    public void onSucceeded(DetailResponse<Sheet> data) {
+                        next(data.getData());
+                    }
+                });
+    }
+
+    /**
+     * 显示数据
+     *
+     * @param data
+     */
+    private void next(Sheet data) {
+        this.data = data;
+        LogUtil.d(TAG, "next :" + data);
     }
 }
 
