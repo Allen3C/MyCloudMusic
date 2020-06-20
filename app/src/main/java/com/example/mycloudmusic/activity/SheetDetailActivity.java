1f25466333ca41983375d67eae441e3974bde82f
allen <allen3c@163.com>
2020-06-20 17:02:47 +0800

    优化收藏后的状态更新

--- app/src/main/java/com/example/mycloudmusic/activity/SheetDetailActivity.java
+++ app/src/main/java/com/example/mycloudmusic/activity/SheetDetailActivity.java
@@ -459,13 +459,23 @@ public class SheetDetailActivity extends BaseTitleActivity implements View.OnCli
                     .deleteCollect(data.getId())
                     .subscribe(new HttpObserver<Response<Void>>() {
                         @Override
-                        public void onSucceeded(Response<Void> data) {
+                        public void onSucceeded(Response<Void> d) {
                             //弹出提示
                             ToastUtil.successShortToast(R.string.cancel_success);
 
                             //重新加载数据
                             //目的是显示新的收藏状态
-                            fetchData();
+                            //fetchData();
+
+                            //取消收藏成功
+                            //SheetDetailActivity.this.data.setCollection_id(null);
+                            data.setCollection_id(null);
+
+                            //收藏数-1
+                            data.setCollections_count(data.getCollections_count() - 1);
+
+                            //刷新状态
+                            showCollectionStatus();
                         }
                     });
         } else {
@@ -476,13 +486,27 @@ public class SheetDetailActivity extends BaseTitleActivity implements View.OnCli
                     .collect(data.getId())
                     .subscribe(new HttpObserver<Response<Void>>() {
                         @Override
-                        public void onSucceeded(Response<Void> data) {
+                        public void onSucceeded(Response<Void> d) {
                             //弹出提示
                             ToastUtil.successShortToast(R.string.collection_success);
 
                             //重新加载数据
                             //目的是显示新的收藏状态
-                            fetchData();
+                            //fetchData();
+
+                            //收藏状态变更后
+                            //可以重新调用歌单详情界面接口
+                            //获取收藏状态
+                            //但对于收藏来说
+                            //收藏数可能没那么重要
+                            //所以不用及时刷新
+                            data.setCollection_id(1);
+
+                            //收藏数+1
+                            data.setCollections_count(data.getCollections_count() + 1);
+
+                            //刷新状态
+                            showCollectionStatus();
                         }
                     });
         }
