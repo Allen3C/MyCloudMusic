2abeb736df1a524c3db297b420273394333b9fee
allen <allen3c@163.com>
2020-06-20 15:14:15 +0800

    使用Palette获取图片颜色

--- app/src/main/java/com/example/mycloudmusic/activity/SheetDetailActivity.java
+++ app/src/main/java/com/example/mycloudmusic/activity/SheetDetailActivity.java
@@ -1,20 +1,29 @@
 package com.example.mycloudmusic.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.mycloudmusic.R;
 import com.example.mycloudmusic.adapter.SongAdapter;
 import com.example.mycloudmusic.domain.Sheet;
@@ -24,6 +33,7 @@ import com.example.mycloudmusic.network.Api;
 import com.example.mycloudmusic.util.Constant;
 import com.example.mycloudmusic.util.ImageUtil;
 import com.example.mycloudmusic.util.LogUtil;
+import com.example.mycloudmusic.util.ResourceUtil;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -171,16 +181,114 @@ public class SheetDetailActivity extends BaseTitleActivity {
             adapter.replaceData(data.getSongs());
         }
 
-        //显示封面
-        if (StringUtils.isBlank(data.getBanner())) {
-            //如果图片为空
+//        //显示封面
+//        if (StringUtils.isBlank(data.getBanner())) {
+//            //如果图片为空
+//
+//            //就用默认图片
+//            iv_banner.setImageResource(R.drawable.dnf);
+//        } else {
+//            //有图片
+//
+//            ImageUtil.show(getMainActivity(), iv_banner, data.getBanner());
+//        }
 
-            //就用默认图片
+
+        //使用Palette获取封面颜色
+        if (StringUtils.isBlank(data.getBanner())) {
+            //图片为空
+
+            //使用默认图片
             iv_banner.setImageResource(R.drawable.dnf);
         } else {
             //有图片
 
-            ImageUtil.show(getMainActivity(), iv_banner, data.getBanner());
+            //这是一个典型的构建者模式
+            //由于这是项目课程
+            //所以这里不详细讲解设计模式
+            Glide.with(this)
+                    .asBitmap()
+                    .load(ResourceUtil.resourceUri(data.getBanner()))
+
+                    //加载图片到自定义目标
+                    //为什么是自定义目标
+                    //是因为我们要获取Bitmap
+                    //然后获取Bitmap的一些颜色
+                    .into(new CustomTarget<Bitmap>() {
+
+                        /**
+                         * 资源加载完成调用
+                         *
+                         * @param resource
+                         * @param transition
+                         */
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            //显示封面
+                            iv_banner.setImageBitmap(resource);
+
+                            //在Material Design(MD，材料设计，是Google的一门设计语言)的设计中
+                            //所谓的设计语言就是一些设计规范
+                            //目前Google已经应用到Android，Gmail等产品
+
+                            //推荐我们将应用的状态栏
+                            //标题栏的颜色和当前页面的内容融合
+                            //也就说当前页面显示一张红色的图片
+                            //那么最好状态栏，标题栏的颜色也和红色差不多
+                            //实现这种效果可以借助的Palette类。
+                            //Palette:可以翻译为调色板
+                            //功能是可以从图片中获取一些颜色
+                            //详细的可以学习《详解Material Design，http://www.ixuea.com/courses/9》课程
+                            Palette.from(resource)
+                                    .generate(new Palette.PaletteAsyncListener() {
+
+                                        /**
+                                         * 颜色计算完成了
+                                         * @param palette
+                                         */
+                                        @Override
+                                        public void onGenerated(@Nullable Palette palette) {
+                                            //获取 有活力 的颜色
+                                            Palette.Swatch swatch = palette.getVibrantSwatch();
+
+                                            //可能没有值所以要判断
+                                            if (swatch != null) {
+                                                //获取颜色的rgb
+                                                int rgb = swatch.getRgb();
+
+                                                //设置标题颜色
+                                                toolbar.setBackgroundColor(rgb);
+
+                                                //设置头部容器颜色
+                                                ll_header.setBackgroundColor(rgb);
+
+                                                //这些api只有高版本才有
+                                                //所以说要判断
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                                    //设置状态栏颜色
+                                                    Window window = getWindow();
+
+                                                    window.setStatusBarColor(rgb);
+
+                                                    //设置导航栏颜色
+                                                    window.setNavigationBarColor(rgb);
+                                                }
+                                            }
+                                        }
+                                    });
+                        }
+
+                        /**
+                         * 加载任务取消了
+                         * 可以在这里释放我们定义的一些资源
+                         *
+                         * @param placeholder
+                         */
+                        @Override
+                        public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                        }
+                    });
         }
 
         //显示标题
