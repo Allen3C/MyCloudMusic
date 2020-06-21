82a2f16aa49fd62cf364d6c5ab8d1f1dcaa6dbfe
allen <allen3c@163.com>
2020-06-21 17:05:06 +0800

    将service设置为前台服务

--- app/src/main/java/com/example/mycloudmusic/service/MusicPlayerService.java
+++ app/src/main/java/com/example/mycloudmusic/service/MusicPlayerService.java
@@ -1,13 +1,16 @@
 package com.example.mycloudmusic.service;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
 import com.example.mycloudmusic.manager.MusicPlayerManager;
 import com.example.mycloudmusic.manager.impl.MusicPlayerManagerImpl;
 import com.example.mycloudmusic.util.LogUtil;
+import com.example.mycloudmusic.util.NotificationUtil;
 import com.example.mycloudmusic.util.ServiceUtil;
 
 /**
@@ -62,6 +65,21 @@ public class MusicPlayerService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         LogUtil.d(TAG, "onStartCommand");
+
+        //因为这个API是8.0才有的
+        //所以要这样判断版本
+        //不然低版本会崩溃
+        if(Build.VERSION.SDK_INT>=26){
+            //设置service为前台service
+
+            //获取通知
+            Notification notification= NotificationUtil.getServiceForeground(getApplicationContext());
+
+            //Id写0：这个通知就不会显示
+            //对于我们这里来说
+            //就需要不显示
+            startForeground (0,notification);
+        }
 
         return super.onStartCommand(intent, flags, startId);
     }
