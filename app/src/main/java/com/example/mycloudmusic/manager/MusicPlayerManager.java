8d1bf8fa60fe1babdeca493b4f21d85c8c79dc82
allen <allen3c@163.com>
2020-06-21 17:21:03 +0800

    实现音乐的播放和暂停

--- app/src/main/java/com/example/mycloudmusic/manager/MusicPlayerManager.java
+++ app/src/main/java/com/example/mycloudmusic/manager/MusicPlayerManager.java
@@ -1,7 +1,31 @@
 package com.example.mycloudmusic.manager;
 
+import com.example.mycloudmusic.domain.Song;
+
 /**
  * 音乐播放器对外暴露的接口
  */
 public interface MusicPlayerManager {
+    /**
+     * 播放音乐
+     * @param uri 播放音乐的绝对地址
+     * @param data
+     */
+    void play(String uri, Song data);
+
+    /**
+     * 是否在播放
+     * @return
+     */
+    boolean isPlaying();
+
+    /**
+     * 暂停
+     */
+    void pause();
+
+    /**
+     *继续播放
+     */
+    void resume();
 }
--- app/src/main/java/com/example/mycloudmusic/manager/impl/MusicPlayerManagerImpl.java
+++ app/src/main/java/com/example/mycloudmusic/manager/impl/MusicPlayerManagerImpl.java
@@ -1,8 +1,12 @@
 package com.example.mycloudmusic.manager.impl;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 
+import com.example.mycloudmusic.domain.Song;
 import com.example.mycloudmusic.manager.MusicPlayerManager;
+
+import java.io.IOException;
 
 /**
  * 播放管理器默认实现
@@ -17,13 +21,25 @@ public class MusicPlayerManagerImpl implements MusicPlayerManager {
      * 上下文
      */
     private final Context context;
+    private final MediaPlayer player;
+    /**
+     * 当前播放的音乐对象
+     */
+    private Song data;
 
     /**
-     * 构造方法
+     * 私有构造方法
+     *
+     * 这里外部就不能通过new方法来创建对象了
      * @param context
      */
     private MusicPlayerManagerImpl(Context context) {
-        this.context = context.getApplicationContext();
+        //保存context
+        //因为后面可能用到
+        this.context = context;
+
+        //初始化播放器
+        player = new MediaPlayer();
     }
 
     /**
@@ -43,5 +59,51 @@ public class MusicPlayerManagerImpl implements MusicPlayerManager {
 
         //返回对象
         return instance;
+    }
+
+    @Override
+    public void play(String uri, Song data) {
+        try {
+            //保存音乐对象
+            this.data = data;
+
+            //释放player
+            player.reset();
+
+            //设置数据源
+            player.setDataSource(uri);
+
+            //同步准备
+            //真实项目中可能会使用异步
+            //因为如果网络不好
+            //同步可能会卡主
+            player.prepare();
+
+            //开始播放
+            player.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return player.isPlaying();
+    }
+
+    @Override
+    public void pause() {
+        if (isPlaying()) {
+            //如果在播放就暂停
+            player.pause();
+        }
+    }
+
+    @Override
+    public void resume() {
+        if (!isPlaying()) {
+            //如果没有播放就播放
+            player.start();
+        }
     }
 }
